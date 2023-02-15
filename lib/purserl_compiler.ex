349fa617dc3c661e2b5f00d026c1b77d1c72c4d7
drathier <github@fi.fo>
2023-02-15 16:45:57 +0100

    Allow passing purs args via config. Only run erlang compiler on erlang files.

--- lib/purserl_compiler.ex
+++ lib/purserl_compiler.ex
@@ -9,8 +9,8 @@ defmodule Mix.Tasks.Compile.Purserl do
 
   @impl Mix.Task.Compiler
   def run(_argv) do
-    #config = Mix.Project.config() |> Keyword.get(:purerlex, Keyword.new())
-    {:ok, pid} = P.start_link()
+    config = Mix.Project.config() |> Keyword.get(:purserl, Keyword.new())
+    {:ok, pid} = P.start_link(config)
     P.trigger_recompile(pid)
   end
 end
