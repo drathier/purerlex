d4fb86aefadaa635370ef5943404af553591877a
drathier <github@fi.fo>
2023-03-26 16:29:30 +0200

    Bump version

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Purerlex.MixProject do
   def project do
     [
       app: :purerlex,
-      version: "0.7.3",
+      version: "0.7.4",
       elixir: "~> 1.11",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
