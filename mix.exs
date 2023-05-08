296baac1458edd29f715bf14f9f8019f4e55d9ee
drathier <github@fi.fo>
2023-05-08 16:58:02 +0200

    Bump tag

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Purerlex.MixProject do
   def project do
     [
       app: :purerlex,
-      version: "0.8.4",
+      version: "0.8.5",
       elixir: "~> 1.11",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
