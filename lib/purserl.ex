42630f8076b4bc9d50bef88e19f91c25fc0152ca
drathier <github@fi.fo>
2026-01-13 15:44:55 +0100

    Allow filtering out errors, so e.g. Shell.purs unused imports and debug warnings can be filtered out like so:
    ```
            filter_warnings: fn x ->
              cond do
                x["moduleName"] == "Shell" && x["errorCode"] == "UnusedImport" -> false
                x["moduleName"] == "Shell" && x["errorCode"] == "UserDefinedWarning" && String.contains?(x["message"], "Debug function usage") -> false
                true -> true
              end
            end,
    ```

--- lib/purserl.ex
+++ lib/purserl.ex
@@ -41,7 +41,7 @@ defmodule Purserl do
       purs_files: config |> Keyword.get(:purs_files, nil),
       purs_cmd: config |> Keyword.get(:purs_cmd, nil),
       extract_cmd: config |> Keyword.get(:purs_cmd, "") == "",
-      filter_warnings: config |> Keyword.get(:filter_warnings, fn x -> true end),
+      filter_warnings: config |> Keyword.get(:filter_warnings, fn _ -> true end),
       purs_args: config |> Keyword.get(:purs_args, ""),
       ctx_lines_above: config |> Keyword.get(:ctx_lines_above, 3) |> (fn x -> x + 1 end).(),
       ctx_lines_below: config |> Keyword.get(:ctx_lines_below, 3) |> (fn x -> x + 1 end).(),
